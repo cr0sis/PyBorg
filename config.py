42e3b674ff57f46b6d38addb512084461c801c1a
cr0sis <64103294+cr0sis@users.noreply.github.com>
2022-10-26 21:00:51 +0000

    Add files via upload

---
+++ config.py
@@ -0,0 +1,10 @@
+HOST = "irc.libera.chat"
+PORT = 6667
+NICK = "cr0bot"                                    #
+PASS = 'derp'
+USER = "cr0bot@cr0sis 0 * timothy"
+CHAN = "#bakedbeans"
+RATE = 0.6                                # messages per seccond
+COMMANDS = [
+    [r".bots", "Reporting in! [Python] Vomit and turd also piss"], 
+]
