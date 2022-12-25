86b396c5d71a2ba39c2673a0c8e56fc72fb389b8
cr0sis <64103294+cr0sis@users.noreply.github.com>
2022-12-25 13:43:32 +0000

    Update utility.py
    more commands

--- utility.py
+++ utility.py
@@ -1,6 +1,8 @@
 import config
 import socket
 import time
+import random
+from datetime import date
 
 def chat(sock, msg):
     """
@@ -11,21 +13,37 @@ def chat(sock, msg):
     """
     sock.send(("PRIVMSG {} :{}\r\n".format(config.CHAN, msg)).encode("UTF-8"))
 
-def ban(sock, user):
-    """
-    Ban a user from the current channel.
-    Keyword arguments:
-    sock -- the socket over which to send the ban command
-    user -- the user to be banned
-    """
-    chat(sock, ".ban {}".format(user))
+def report_in(u, m):
+    return "Reporting *hic* in! [" + u'\U0001F40D' + "] https://cr0s.is/cr0bot.php"
 
-def timeout(sock, user, secs=600):
-    """
-    Time out a user for a set period of time.
-    Keyword arguments:
-    sock -- the socket over which to send the timeout command
-    user -- the user to be timed out
-    secs -- the length of the timeout in seconds (default 600)
-    """
-    chat(sock, ".timeout {}".format(user, secs))
+def check_time(u, m):
+    curr_time = time.strftime("%H:%M:%S", time.localtime())
+    return curr_time
+
+
+def check_date(u, m):
+    today = date.today()
+    d2 = today.strftime("%B %d, %Y")
+    return d2
+
+def jumble(word, m):
+    word = str("GINGER")
+    word = list(word)  # Convert the word to a list of characters
+    random.shuffle(word)  # Shuffle the list of characters
+    return ''.join(word)  # Join the list of characters back into a string and return it
+
+
+def roll_dice(u, m):
+    dice = [] # Initialize an empty list to store the dice rolls
+    for i in range(7):
+        roll = random.randint(1, 6)
+        if roll == 1:
+            dice.append("\x0313,15" + str(roll)) # Append the rolled value with IRC color codes for white background and pink text
+        else:
+            dice.append("\x0301,15" + str(roll)) # Append the rolled value with IRC color codes for white background and black text
+        dice_str = " ".join(dice)
+        int_dice = [int(x[6:]) for x in dice] # Remove the IRC color codes and convert to integer
+        if i == 6:
+            sum_dice = sum(int_dice)
+            dice_str = "\x0301,15-".join(dice)
+            return dice_str + "\x03 " + u + "\x03 rolled: \x0307" + str(sum_dice)
