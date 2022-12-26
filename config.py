1290d64b973b5ec2872b74ba2fb74995848cd869
cr0sis <64103294+cr0sis@users.noreply.github.com>
2022-12-26 23:51:18 +0000

    Update config.py
    more commands, fixed rate

--- config.py
+++ config.py
@@ -4,11 +4,18 @@ NICK = "yourbot"                                    #
 PASS = 'derp'
 USER = "yourbot@hisname 0 * hisname"
 CHAN = "#chan"
-RATE = 0.6                                # messages per seccond
+RATE = (20/30)                                # messages per seccond
 COMMANDS = [
-    [r':\.bots\r\n$', utility.report_in],
-    [r':!roll7\r\n$', utility.roll_dice],
-    [r':!time\r\n$', utility.check_time], 
-    [r':!ginger\r\n$', utility.jumble],
-    [r':!date\r\n$', utility.check_date],
+    [r':\.bots$', utility.report_in],
+    [r':~roll7$', utility.roll_dice],
+    [r':~time$', utility.check_time], 
+    [r':~ginger$', utility.jumble],
+    [r':~date$', utility.check_date],
+    [r':~temp$', utility.temp],
+    [r':~iss$', utility.ISS],
+    [r':~random', utility.random_choice],
+    [r':~dog$', utility.dogs],
+    [r':~fox$', utility.fox],
+    [r':~duck$', utility.duck],
+    [r':~bankhol$', utility.bankhol],
 ]
