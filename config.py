346f1aaddbc15c61cacc5732b79b110e835774c3
cr0sis <64103294+cr0sis@users.noreply.github.com>
2022-12-29 08:41:28 +0000

    Update config.py
    updated regexing to match new message parsing routine

--- config.py
+++ config.py
@@ -7,16 +7,16 @@ USER = "yourbot@hisname 0 * hisname"
 CHAN = "#chan"
 RATE = (20/30)                                # messages per seccond
 COMMANDS = [
-    [r':\.bots$', utility.report_in],
-    [r':~roll7$', utility.roll_dice],
-    [r':~time$', utility.check_time], 
-    [r':~ginger$', utility.jumble],
-    [r':~date$', utility.check_date],
-    [r':~temp$', utility.temp],
-    [r':~iss$', utility.ISS],
-    [r':~random', utility.random_choice],
-    [r':~dog$', utility.dogs],
-    [r':~fox$', utility.fox],
-    [r':~duck$', utility.duck],
-    [r':~bankhol$', utility.bankhol],
+    [r'^\.bots$', utility.report_in],
+    [r'^~roll7$', utility.roll_dice],
+    [r'^~time$', utility.check_time],
+    [r'^~ginger$', utility.jumble],
+    [r'^~date$', utility.check_date],
+    [r'^~temp$', utility.temp],
+    [r'^~iss$', utility.ISS],
+    [r'^~random', utility.random_choice],
+    [r'^~dog$', utility.dogs],
+    [r'^~fox$', utility.fox],
+    [r'^~duck$', utility.duck],
+    [r'^~bankhol$', utility.bankhol],
 ]
