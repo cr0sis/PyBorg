14df62482b569d3bdb094dcd3e188d7b57cad794
cr0sis <64103294+cr0sis@users.noreply.github.com>
2023-01-22 21:23:35 +0000

    Update main.py

--- main.py
+++ main.py
@@ -5,22 +5,20 @@ import socket
 import time
 import re
 
-CHAT_MSG = re.compile(r"^:\w+!\w+@\w+\.tmi\.twitch\.tv PRIVMSG #\w+ :")
-
 try:
     s = socket.socket()
     s.connect((config.HOST, config.PORT))
-    s.send(("NICK {}\r\n".format(config.NICK)).encode("utf-8"))
-    s.send(("PASS {}\r\n".format(config.PASS)).encode("utf-8"))
-    s.send(("USER {}\r\n".format(config.USER)).encode("utf-8"))
     connected = True #Socket succefully connected
+    s.send(("USER {}\n".format(config.USER)).encode("utf-8"))
+    s.send(("NICK {}\n".format(config.NICK)).encode("utf-8"))
 
 except Exception as e:
     print(str(e))
+    print('there has been a problemo')
     connected = False #Socket failed to connect
 
 def parse_message(line):
-    res = re.search(r":(.*)\!\~(.*) (.*) (.*) :(.*)", line)
+    res = re.search(r":(.*)\!\~?(.*) (.*) (.*) :(.*)", line)
     if(res != None):
         m = {
           'user': res.group(1),
@@ -35,28 +33,30 @@ def parse_message(line):
         return False
 
 def bot_loop():
+    joined = False
     while connected:
-        for channel in config.CHANNELS:
-            s.send(("JOIN {}\r\n".format(channel)).encode("utf-8"))
-            try:
-                response = s.recv(1024).decode("utf-8")
-            except Exception as e:
-                print("An error occurred while receiving a message:", str(e))
-                break
-            if response[0:4] == "PING":
-                print(response)
-                s.send(("PONG " + response.split()[1] + "\r\n").encode("utf-8"))
-                print("Pong")
-            else:
-                response = response.rstrip("\r\n")
-                print(response.encode("utf-8"))
-                message = parse_message(response)
-                if(message != False):
-                    print((message["user"] + ": " + message["message"]).encode("utf-8"))
-                    for pattern in config.COMMANDS:
-                        if re.search(pattern[0], message["message"]):
-                            utility.chat(s, pattern[1](message), message["channel"])
-                            break
+        # try:
+        response = s.recv(1024).decode("utf-8")
+        print(response)
+        if response[0:4] == "PING":
+            print("PONG " + response.split()[1] + "\r\n")
+            s.send(("PONG " + response.split()[1] + "\r\n").encode("utf-8"))
+            if (joined == False):
+                s.send(("PASS {}\r\n".format(config.PASS)).encode("utf-8"))
+                for channel in config.CHANNELS:
+                  s.send(("JOIN {}\r\n".format(channel)).encode("utf-8"))
+                  joined = True
+        else:
+            response = response.rstrip("\r\n")
+            message = parse_message(response)
+#           print(message.encode("utf-8"))
+            if(message != False):
+                print((message["user"] + ": " + message["message"]).encode("utf-8"))
+                for pattern in config.COMMANDS:
+                    if re.search(pattern[0], message["message"]):
+                        utility.chat(s, pattern[1](message), message["channel"])
+                        break
+      
 if __name__ == "__main__":
     try:
         bot_loop()
